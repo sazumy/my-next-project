b11daa8b0a97d425d412e3816a7fdb4473a3fb62
Azumi S <azumisaito@gmail.com>
2024-07-21 20:48:01 +0900

    ボタンの追加

---
+++ app/_components/ButtonLink/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./index.module.css";
+
+type Props = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const ButtonLink = ({ href, children }: Props) => {
+  return (
+    <a href={href} className={styles.button}>
+      {children}
+    </a>
+  );
+};
+
+export default ButtonLink;
