4435ecfa2d5b4af6743e80458cf975c0891ec6f0
Azumi S <azumisaito@gmail.com>
2024-08-20 17:26:08 +0900

    不正なカテゴリーが入力された時の処理を追加

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { getNewsList } from "@/app/_libs/microcms";
+import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
+import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
 
 type Props = {
@@ -8,8 +9,9 @@ type Props = {
 };
 
 const Page = async ({ params }: Props) => {
+  const category = await getCategoryDetail(params.id).catch(notFound);
   const { contents: news } = await getNewsList({
-    filters: `category[equals]${params.id}`,
+    filters: `category[equals]${category.id}`,
   });
 
   return <NewsList news={news} />;
