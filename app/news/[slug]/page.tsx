ea73ea21f228be60c729316eb4ae364b3647086d
Azumi S <azumisaito@gmail.com>
2024-08-23 17:47:53 +0900

    下書き機能の追加

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -6,10 +6,13 @@ import styles from "./page.module.css";
 
 type Props = {
   params: { slug: string };
+  searchParams: { dk?: string };
 };
 
-const Page = async ({ params }: Props) => {
-  const data = await getNewsDetail(params.slug).catch(notFound);
+const Page = async ({ params, searchParams }: Props) => {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
+  }).catch(notFound);
 
   return (
     <>
