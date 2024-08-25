82a5f3f1b112417b5353fd5cf2328cd97ac155f1
Azumi S <azumisaito@gmail.com>
2024-08-25 10:17:54 +0900

    WIP 検索機能の追加

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import styles from "./index.module.css";
 
 export const SearchField = () => {
+  const router = useRouter();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const q = event.currentTarget.elements.namedItem("q");
+    if (q instanceof HTMLInputElement) {
+      const params = new URLSearchParams();
+      params.set("q", q.value.trim());
+      router.push(`/news/search?${params.toString()}`);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <label htmlFor="" className={styles.search}>
         <Image src="/search.svg" alt="検索" width={16} height={16} />
         <input
