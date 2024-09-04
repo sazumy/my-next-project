6c84c5aacc1988d67f27d6a2b3721a4cd2af755e
Azumi S <azumisaito@gmail.com>
2024-09-04 20:46:37 +0900

    basic認証に関する記述を削除

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { match } from "assert";
-import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
-
-export const middleware = createNextAuthMiddleware();
-
-// NOTE: 右辺の{}はマッチャーと呼ばれ、空のオブジェクトを指定することで、全てのリクエストに対してこのmiddlewareを適用することを意味します。
-export const config = { matcher: ["/(.*)"] };
