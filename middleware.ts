fa4efe25308fdf627e5ddc408eb51bff9ded72e7
Azumi S <azumisaito@gmail.com>
2024-09-04 20:40:24 +0900

    Basic認証の設定

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { match } from "assert";
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-export const middleware = (request: NextRequest) => {
-  console.log("middleware: " + request.url);
-
-  return NextResponse.next();
-};
+export const middleware = createNextAuthMiddleware();
 
 // NOTE: 右辺の{}はマッチャーと呼ばれ、空のオブジェクトを指定することで、全てのリクエストに対してこのmiddlewareを適用することを意味します。
-export const config = {};
+export const config = { matcher: ["/(.*)"] };
