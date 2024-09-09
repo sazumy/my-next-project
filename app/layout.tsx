926f415ce12c0e43bda13fdfef7000abe84153c5
Azumi S <azumisaito@gmail.com>
2024-09-09 21:07:22 +0900

    トップページのメタデータを設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,27 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト", // %s にページタイトルが入る
+    default: "シンプルなコーポレートサイト",
+  },
+  description:
+    "「Next.js+ヘッドレスCMSではじめる！かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    // ソーシャルメディア上での表示設定
+    title: "シンプルなコーポレートサイト",
+    description:
+      "「Next.js+ヘッドレスCMSではじめる！かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "http://localhost:3000",
+  },
+};
 
 export default function RootLayout({
   children,
